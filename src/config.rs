da65e69af4bc6f78fb030de2e4c2c0fc3a82b4a4
Tomasz Mazur <mazur.tomasz34@gmail.com>
2023-04-22 22:34:53 +0200

    Environment changes

--- src/config.rs
+++ src/config.rs
@@ -11,6 +11,23 @@ pub struct Config {
 
 const CONFIG_PATH: &str = "config.json";
 
+pub const SERVER_ADDR_ENV_VAR: &str = "SERVER_ADDR";
+pub const DATABASE_ADDR_ENV_VAR: &str = "DATABASE_ADDR";
+
+fn load_config() -> Config {
+    let mut cfg: Config = serde_json::from_reader(BufReader::new(File::open(Path::new(CONFIG_PATH)).unwrap())).unwrap();
+
+    if let Ok(server_addr) = std::env::var(SERVER_ADDR_ENV_VAR) {
+        cfg.server_addr = server_addr.parse().unwrap();
+    }
+
+    if let Ok(database_addr) = std::env::var(DATABASE_ADDR_ENV_VAR) {
+        cfg.database_addr = database_addr.parse().unwrap();
+    }
+
+    cfg
+}
+
 lazy_static! {
-    pub static ref CONFIG: Config = serde_json::from_reader(BufReader::new(File::open(Path::new(CONFIG_PATH)).unwrap())).unwrap();
+    pub static ref CONFIG: Config = load_config();
 }
